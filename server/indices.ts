bc728456d6f87d5dae42f45aa35f935aa3a91545
Sadhbh Code <sonia.kolasinska.pro@gmail.com>
2025-09-23 20:57:47 +0100

    Temporarily use QA index

--- server/indices.ts
+++ server/indices.ts
@@ -6,7 +6,7 @@ import axios from "axios";
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_API;
 
-export const fetchAllIndices = async (): Promise<IndexListEntry[]> => {
+export const fetchAllIndicesProd = async (): Promise<IndexListEntry[]> => {
   const response = await fetch(`${API_BASE_URL}/indices/getIndexLists`);
 
   if (!response.ok) {
@@ -15,7 +15,43 @@ export const fetchAllIndices = async (): Promise<IndexListEntry[]> => {
   }
 
   return response.json();
+}
+
+export const fetchAllIndicesQa = async (): Promise<IndexListEntry[]> => {
+  return [{
+    "indexId": 21,
+    "name": "SY100",
+    "address": "0x9080dd35d88b7de97afd0498fc309784ef7ebc49",
+    "ticker": "SY100",
+    "curator": "0xF7F7d5C0d394f75307B4D981E8DE2Bab9639f90F",
+    "totalSupply": 0.00002010588139611647,
+    "totalSupplyUSD": 6.195548738217032,
+    "ytdReturn": -11.49,
+    "collateral": [
+      { "name": "BTC", "logo": "https://coin-images.coingecko.com/coins/images/1/thumb/bitcoin.png?1696501400" },
+      { "name": "ETH", "logo": "https://coin-images.coingecko.com/coins/images/279/thumb/ethereum.png?1696501628" },
+      { "name": "XRP", "logo": "https://coin-images.coingecko.com/coins/images/44/thumb/xrp-symbol-white-128.png?1696501442" },
+      { "name": "SOL", "logo": "https://coin-images.coingecko.com/coins/images/4128/thumb/solana.png?1718769756" },
+      { "name": "BNB", "logo": "https://coin-images.coingecko.com/coins/images/825/thumb/bnb-icon2_2x.png?1696501970" },
+      { "name": "DOGE", "logo": "" }
+    ],
+    "managementFee": 2,
+    "assetClass": "Cryptocurrencies",
+    "category": "Top 100 Market-Cap Tokens",
+    "inceptionDate": "2019-01-01",
+    "performance": {
+      "ytdReturn": -11.49,
+      "oneYearReturn": 76.38137132434154,
+      "threeYearReturn": 237.1885256621526,
+      "fiveYearReturn": 1738.3370284019127,
+      "tenYearReturn": 0
+    },
+    "ratings": { "overallRating": "A+", "expenseRating": "B", "riskRating": "C+" },
+    "indexPrice": 308146.09
+  }];
 };
+
+export const fetchAllIndices = fetchAllIndicesQa;
 
 export const deposit = async (address: string, amount: string): Promise<any> => {
   const response = await fetch(`${API_BASE_URL}/indices/deposit/${address}/${amount}`);
@@ -115,7 +151,7 @@ export const fetchHistoricalData = async (
     log.error("Failed to fetch historical data", { status: response.status, statusText: response.statusText });
     return null
   }
-  else{
+  else {
     return response.json();
   }
 };
