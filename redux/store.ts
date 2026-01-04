4ffa1647e225c055a03a77de64c7a02c0de96e54
MAX-OTC <maxgctr@gmail.com>
2026-01-04 18:13:45 +0100

    clean component with redux

--- redux/store.ts
+++ redux/store.ts
@@ -7,6 +7,7 @@ import walletReducer from "./walletSlice";
 import assetReducer from "./assetSlice";
 import mintInvoiceReducer from "./mintInvoicesSlice";
 import marketDataReducer from './market-data-slice';
+import vaultDetailReducer from './vaultDetailSlice';
 
 export const store = configureStore({
   reducer: {
@@ -18,6 +19,7 @@ export const store = configureStore({
     assets: assetReducer,
     mintInvoices: mintInvoiceReducer,
     marketData: marketDataReducer,
+    vaultDetail: vaultDetailReducer,
   },
 });
 
